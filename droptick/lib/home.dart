73894c5c751a19930b23d032aec176dc7429f240
DanielAndresC <danielandres300what@gmail.com>
2022-04-22 16:43:09 +0200

    Comprobación conexión

--- droptick/lib/home.dart
+++ droptick/lib/home.dart
@@ -7,6 +7,8 @@ class PaginaHome extends StatefulWidget {
   State<PaginaHome> createState() => _PaginaHomeState();
 }
 
+//El paquete del SDK sólo llega a 2.14.2., no más allá
+
 class _PaginaHomeState extends State<PaginaHome> {
   @override
   Widget build(BuildContext context) {
