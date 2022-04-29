26b19e975d47c6f568fe000a66863b049b919709
DanielAndresC <danielandres300what@gmail.com>
2022-04-29 19:24:17 +0200

    File Foto

---
+++ droptick/lib/foto.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Camara extends StatefulWidget {
+  @override
+  _EstadoCamara createState() => _EstadoCamara();
+}
+
+class _EstadoCamara extends State<Camara> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
