31ff1956d1a091eed379d93a8494f4be6eb5a8f0
DanielAndresC <danielandres300what@gmail.com>
2022-04-29 20:14:33 +0200

    Foto cambios

--- droptick/lib/foto.dart
+++ droptick/lib/foto.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
 class Camara extends StatefulWidget {
   @override
@@ -6,6 +7,55 @@ class Camara extends StatefulWidget {
 }
 
 class _EstadoCamara extends State<Camara> {
+  
+  CameraController? controller;
+  bool _isCameraInitialized = false;
+  
+  void onNewCameraSelected(CameraDescription cameraDescription) async {
+      final previousCameraController = controller;
+      // Instantiating the camera controller
+      final CameraController cameraController = CameraController(
+        cameraDescription,
+        ResolutionPreset.high,
+        imageFormatGroup: ImageFormatGroup.jpeg,
+      );
+
+      // Dispose the previous controller
+      await previousCameraController?.dispose();
+
+      // Replace with the new controller
+      if (mounted) {
+         setState(() {
+           controller = cameraController;
+        });
+      }
+
+      // Update UI if controller updated
+      cameraController.addListener(() {
+        if (mounted) setState(() {});
+      });
+
+      // Initialize controller
+      try {
+        await cameraController.initialize();
+      } on CameraException catch (e) {
+        print('Error initializing camera: $e');
+      }
+
+      // Update the Boolean
+      if (mounted) {
+        setState(() {
+           _isCameraInitialized = controller!.value.isInitialized;
+        });
+      }
+  }
+
+  @override
+  void initState() {
+    onNewCameraSelected(cameras[0]);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
